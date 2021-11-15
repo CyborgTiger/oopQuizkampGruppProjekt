dbabd4bfe3a7fc2435bed2f10550cba46d8bba51
JohanssonJacob <jacobjohanssn2001@gmail.com>
2021-11-15 14:47:00 +0100

    Basic Game GUI

---
+++ src/Client/GameGUI.java
@@ -0,0 +1,74 @@
+package Client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameGUI extends JFrame implements ActionListener {
+
+    JPanel questionPanel = new JPanel();
+    JPanel optionPanel = new JPanel();
+    JButton optionOne = new JButton("optionOne");
+    JButton optionTwo = new JButton("optionTwo");
+    JButton optionThree = new JButton("optionThree");
+    JButton optionFour = new JButton("optionFour");
+    JLabel questionLabel = new JLabel("Lasdasd Oasdasd Qsdasd Asdasa Olasdasd?");
+
+    public GameGUI(){
+
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setTitle("KampQuizen");
+        setSize(700,800);
+        add(questionPanel, BorderLayout.NORTH);
+        //questionPanel.setPreferredSize(new Dimension(300,300));
+        optionPanel.setPreferredSize(new Dimension(600,400));
+        optionPanel.setLayout(new GridLayout(2,2));
+        add(optionPanel, BorderLayout.CENTER);
+
+        questionLabel.setFont(new Font("Helvetica", Font.PLAIN, 37));
+        questionPanel.add(questionLabel, BorderLayout.SOUTH);
+
+        addOptions();
+
+        pack();
+    }
+
+    public static void main(String[] args) {
+        GameGUI gU = new GameGUI();
+        gU.setVisible(true);
+    }
+
+    public void createButtons() {
+        Dimension buttonSize = new Dimension(100,50);
+        optionOne.setPreferredSize(buttonSize);
+        optionTwo.setPreferredSize(buttonSize);
+        optionThree.setPreferredSize(buttonSize);
+        optionFour.setPreferredSize(buttonSize);
+
+    }
+
+    public void addOptions() {
+        createButtons();
+        optionPanel.add(optionOne);
+        optionPanel.add(optionTwo);
+        optionPanel.add(optionThree);
+        optionPanel.add(optionFour);
+        optionOne.addActionListener(this);
+        optionTwo.addActionListener(this);
+        optionThree.addActionListener(this);
+        optionFour.addActionListener(this);
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() != null) {
+            System.out.println(e.getSource().toString());
+        }
+
+    }
+}
