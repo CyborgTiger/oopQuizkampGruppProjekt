9b5b61517a2765cefb670ba764305d7395c88d2e
Tiago Fonseca <tiago.godinho.email@gmail.com>
2021-11-24 14:00:43 +0100

    quiz categories addition

--- src/Server/QuizCategories.java
+++ src/Server/QuizCategories.java
@@ -23,11 +23,17 @@ public class QuizCategories {
         ArrayList<QuizQuestion> animals = new ArrayList<>();
         animals.add(new QuizQuestion("Where are tigers originally from?",2, "Africa", "South America", "Asia", "Europe"));
         animals.add(new QuizQuestion("Where can you find emperor penguins?",3, "Arctic", "Australia", "Greenland", "Antarctica"));
+        animals.add(new QuizQuestion("Which of these animals is the fastest land animal?",0, "Falcon", "Sail Fish", "Turtle", "Cheetah"));
+        animals.add(new QuizQuestion("One of these animals is the biggest on this planet, which one?",1, "African Elephant", "Blue Whale", "Whale Shark", "Girafffe"));
+        animals.add(new QuizQuestion("What is a group of hedgehogs called?",3, "Herd", "Skulk", "Bag", "Pickle"));
         quizCategories.add(new QuizQuestions(animals));
 
         ArrayList<QuizQuestion> sport = new ArrayList<>();
         sport.add(new QuizQuestion("Who won the World Football Championship 2002",2, "Germany", "Turkey", "Brazil", "France"));
+        sport.add(new QuizQuestion("Who has been awarded the Ballon d'or the most?",1, "Cristiano Ronaldo", "Lionel Messi", "Diego Maradona", "Pele"));
         sport.add(new QuizQuestion("How many human players does a polo team have",3, "Two", "Six", "Eleven", "Four"));
+        sport.add(new QuizQuestion("Who headbutted another player in the 2006 world cup?",2, "Wayne Rooney", "Marco Materazzi", "Zinedine Zidane", "Zlatan Ibrahimovic"));
+        sport.add(new QuizQuestion("Who was the highest paid athlete in 2021?",0, "Connor McGregor", "Lionel Messi", "Roger Federer", "LeBron James"));
         quizCategories.add(new QuizQuestions(sport));
 
         ArrayList<QuizQuestion> food = new ArrayList<>();
@@ -45,6 +51,14 @@ public class QuizCategories {
         swedishHistory.add(new QuizQuestion("Where was Sweden's first university grounded?",0, "Uppsala", "Lund", "Stockholm", "Åbo"));
         swedishHistory.add(new QuizQuestion("What year did Queen Silvia and King Carl-Gustav marry?",2, "1977", "1982", "1976", "1979"));
         quizCategories.add(new QuizQuestions(swedishHistory));
+
+        ArrayList<QuizQuestion> music = new ArrayList<>();
+        music.add(new QuizQuestion("Which singer was known amongst other things as 'The King of Pop'?",3, "Elvis Presley", "Elton John", "Frank Sinatra", "Micheal Jackson"));
+        music.add(new QuizQuestion("They took on the world with the massive hit 'Love Fool', which band?",0, "ABBA", "Cardigans", "Meja", "Erasure"));
+        music.add(new QuizQuestion("What was Britney Spears’ first single called?",2, "Love you Baby", "I need you Baby", "Baby one more time", "My Baby"));
+        music.add(new QuizQuestion("What is David Bowie’s real name?",0, "David Jones", "David Evans", "David Robin", "David Stefan"));
+        music.add(new QuizQuestion("A Swedish duo from the 60s once jammed with Jimi Hendrix. Which duo?",1, "Womack and Womack", "Hansson and Carlsson", "Per and Marie", "Jean and Dean"));
+        quizCategories.add(new QuizQuestions(music));
 
         return quizCategories;
     }
